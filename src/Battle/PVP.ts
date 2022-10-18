06b0e81342fe90c4969efe84950311e8da591b9b
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-18 17:21:02 -0300

    PVP implementada

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  private _firstCharacter: Fighter;
+  private _secondCharacter: Fighter;
+
+  constructor(firstCharacter: Fighter, secondCharacter: Fighter) {
+    super(firstCharacter);
+    this._firstCharacter = firstCharacter;
+    this._secondCharacter = secondCharacter;
+  }
+
+  public fight(): number {
+    if (this._firstCharacter.lifePoints > 0
+      && this._secondCharacter.lifePoints > 0) {
+      this._firstCharacter.attack(this._secondCharacter);
+      this._secondCharacter.attack(this._firstCharacter);
+    }
+    return super.fight();
+  }
+}
